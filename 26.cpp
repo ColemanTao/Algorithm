88ca43304459c8a0e6d33e40c57498a2c4e26922
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-08-21 21:27:46 +0800

    26st version of Algorithm

---
+++ 26.cpp
@@ -0,0 +1,55 @@
+// #include<iostream>
+// #include<cstdio>
+// using namespace std;
+
+// #define P 3.14159
+// int a, b,c;
+// int main() {
+//     cin >> a >> b >> c;
+//     cout << "TRIANGULO: " << a * c;
+//     printf("\n");
+//     cout << "CIRCULO: " << c * c * P;
+//     printf("\n");
+//     cout << "TRAPEZIO: " << (a + b) * c / 2;
+//     printf("\n");
+//     cout << "QUADRADO: " << b * b;
+//     printf("\n");
+//     cout << "RETANGULO: " << a * b;
+//     printf("\n");
+//     return 0;
+// }
+// #include<cstdio>
+// #include<iostream>
+// using namespace std;
+
+// #define P 3.14159
+// int main() {
+//     double a, b, c;
+//     cin >> a >> b >> c;
+//     printf("TRIANGULO: %.3lf\n", a * c /2.0);
+//     printf("CIRCULO: %.3lf\n", P * c * c);
+//     printf("TRAPEZIO: %.3lf\n", (a + b) * c / 2.0);
+//     printf("QUADRADO: %.3lf\n", b * b);
+//     printf("RETANGULO: %.3lf\n", a * b);
+//     return 0;
+// }
+// #include<iostream>
+// #include<cstdio>
+// using namespace std;
+
+// int main() {
+//     double a, b, c;
+//     cin >> a >> b >> c;
+//     printf("MEDIA = %.1lf", (a * 2 + b * 3 + c * 5) / 10.0);
+//     return 0;
+// }
+// #include<cstdio>
+// using namespace std;
+
+// int main() {
+//     char name[5];
+//     double salar1, salar2;
+//     scanf("%s %lf %lf", &name[5], &salar1, &salar2);
+//     printf("TOTAL = R$ %.2lf", salar1 + salar2 * 0.15);
+//     return 0;
+// }
