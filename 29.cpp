ba23493526eb4d4311b8fa827a29c99e9d6bface
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-08-25 21:18:51 +0800

    29st version of Algorithm

---
+++ 29.cpp
@@ -0,0 +1,36 @@
+// #include<iostream>
+// #include<cstring>
+// #include<cstdlib>
+// using namespace std;
+
+// int main() {
+//     char a1[] = "hello";
+//     char a2[] = "#####";
+//     int num = sizeof(a1)/sizeof(a1[0]) - 2;
+//     int num1 = strlen(a1) - 1;
+//     int left = 0, right = num;
+//     for (; left <= right; left++, right++) {
+//         a2[left] = a1[left];
+//         a2[right] = a1[right];
+//         printf("%s\n", a2);
+//         // right--;
+//         // left++;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     char s[20];
+//     char s1[] = "12345";
+//     while (scanf("%d", &s) != EOF) {
+//         if (s == s1) {
+
+//         }
+//     }
+//     return 0;
+// }
+
