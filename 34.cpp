8346b81d72bfce7d2a562c25405f92a2b928611a
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-09-01 22:44:34 +0800

    34st version of Algorithm

---
+++ 34.cpp
@@ -0,0 +1,69 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main()
+// {
+//     int a = 1;
+//     int b = a ++ ;
+
+//     cout << a << ' ' << b << endl;
+
+//     int c = ++ a;
+
+//     cout << a << ' ' << c << endl;
+
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     cout << "Hello world" << endl;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int x, y;
+//     cin >> x >> y;
+//     switch(x) {
+//         case 1: printf("Total: R$ %.2lf", 4.0 * y);; break;
+//         case 2: printf("Total: R$ %.2lf", 4.5 * y);; break;
+//         case 3: printf("Total: R$ %.2lf", 5.0 * y);; break;
+//         case 4: printf("Total: R$ %.2lf", 2.0 * y);; break;
+//         case 5: printf("Total: R$ %.2lf", 1.5 * y);; break;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+
+// int main() {
+//     double a, b, c;
+//     cin >> a >> b >> c;
+//     if ((a + b > c) && fabs(a - b) < c) {
+//         printf("Perimetro = %.lf", a + b + c);
+//     }
+//     else {
+//         printf("Area = %.lf", (a + b)*c/2);
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    return 0;
+}
