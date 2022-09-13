6bfef7210d204c9f8c9fc04e532e08aa9a71151d
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-09-13 22:03:30 +0800

    Add files via upload

---
+++ 38.cpp
@@ -0,0 +1,93 @@
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     double n;
+//     cin >> n;
+//         if (n >= 0 && n <= 25) {
+//             printf("Intervalo[0,25]");
+//         }
+//         else if (25 < n && n <= 50) {
+//             printf("Intervalo(25,50]");
+//         }
+//         else if (50 < n && n <= 75) {
+//             printf("Itervalo(50,75]");
+//         }
+//         else if (75 < n && n <= 100) {
+//             printf("Itervalo(75,100]");
+//         }
+//         else {
+//             printf("Fora de intervalo");
+//         }
+//     return 0;
+// }
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     if (a == b) {
+//         printf("O JOGO DUROU 24 HORA(S)");
+//     }
+//     if (b > a) {
+//         printf("O JOGO DUROU %d HORA(S)", b - a);
+//     }
+//     else {
+//         printf("O JOGO DUROU %d HORA(S)", 24 - b + a);
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     double s;
+//     cin >> s;
+//     if (0 <= s && s <= 400)
+//     {
+//         printf("Novo salario: %.2lf\n", s * 1.15);
+//         printf("Reajuste ganho: %.2lf\n", s * 0.15);
+//         printf("Em percentual: 15 %%");
+//     }
+//     else if (400.01 <= s && s <= 800)
+//     {
+//         printf("Novo salario: %.2lf\n", s * 1.1240040);
+//         printf("Reajuste ganho: %.2lf\n", s * 0.12);
+//         printf("Em percentual: 12 %%");
+//     }
+//     else if (800.01 <= s && s <= 1200)
+//     {
+//         printf("Novo salario: %.2lf\n", s * 1.1);
+//         printf("Reajuste ganho: %.2lf\n", s * 0.1);
+//         printf("Em percentual: 10 %%");
+//     }
+//     else if (1200.01 <= s && s <= 2000)
+//     {
+//         printf("Novo salario: %.2lf\n", s * 1.07);
+//         printf("Reajuste ganho: %.2lf\n", s * 0.07);
+//         printf("Em percentual: 7 %%");
+//     }
+//     else
+//     {
+//         printf("Novo salario: %.2lf\n", s * 1.04);
+//         printf("Reajuste ganho: %.2lf\n", s * 0.04);
+//         printf("Em percentual: 4 %%");
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    char a[20];
+    return 0;
+}
