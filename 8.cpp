2968d32663169c54b3052c17c82c6427b64e1e97
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-09-28 23:14:06 +0800

    Add files via upload

--- 8.cpp
+++ 8.cpp
@@ -1,75 +1,75 @@
+// #include<cstdio>
 // #include<iostream>
 
 // using namespace std;
-// const int N = 1e5+10;
-// int n;
-// int q[N];
-
-// void quick_sort(int q[], int l, int r) {
-//     if (l >= r) return;
-//     int x = q[l + r >> 1], i = l - 1, j = r + 1;
-//     while (i < j) {
-//         while (q[++i] < x);
-//         while (q[--j] > x);
-//         if (i < j) swap(q[i], q[j]);
-//     }
-//     quick_sort(q, l, j);
-//     quick_sort(q, j + 1, r);
-// }
 
 // int main() {
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cin >> q[i];
-//     }
-//     quick_sort(q, 0, n - 1);
-//     for (int i = 0; i < n; i++) {
-//         cout << q[i] << ' ';
+//     return 0;
+//     int num, time;
+//     double salary;
+//     cin >> num >> time >> salary;
+//     printf("NUMBER = %d\nSALARY = U$ %.2lf", num, salary * time);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << a + b << endl;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int a, b;
+//         cin >> a >> b;
+//         cout << a + b << endl;
 //     }
 //     return 0;
 // }
-#include <iostream>
+// #include<iostream>
 
-using namespace std;
+// using namespace std;
 
-const int N = 1e5;
-int n;
-int q[N], tmp[N];
+// int main() {
+//     int a, b;
+//     while (cin >> a >> b) {
+//         cout << a + b << endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
 
-void merge_sort(int q[], int l, int r)
-{
-    if (l >= r)
-        return;
-    int mid = l + r >> 1;
-    merge_sort(q, l, mid), merge_sort(q, mid + 1, r);
-    int k = 0, i = l, j = mid + 1;
-    while (i <= mid && j <= r)
-    {
-        if (q[i] <= q[j])
-            tmp[k++] = q[i++];
-        else
-            tmp[k++] = q[j++];
-    }
-    while (i <= mid)
-        tmp[k++] = q[i++];
-    while (j <= r)
-        tmp[k++] += q[j++];
-    for (int i = l, j = 0; i <= r; i++, j++)
-    {
-        q[i] = tmp[j];
-    }
-}
-int main()
-{
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> q[i];
-    }
-    merge_sort(q, 0, n - 1);
-    for (int i = 0; i < n; i++)
-    {
-        cout << q[i] << ' ';
-    }
-    return 0;
-}
+// using namespace std;
+
+// int main() {
+//     int a, b;
+//     while (cin >> a >> b, a || b) {
+//         cout << a + b << endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int Plu(int n) {
+//     if (n <= 1) {
+//         return 1;
+//     }
+//     else return Plu(n - 1) * n;
+// }
+// int main() {
+//     int n;
+//     cin >> n;
+//     int x = Plu(n);
+//     cout << x << endl;
+//     return 0;
+// }
