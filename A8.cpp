89d24b6faf75f0ce546d8221bcb551cdb9cccf9a
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-09-28 22:51:59 +0800

    Add files via upload

---
+++ A8.cpp
@@ -0,0 +1,75 @@
+// #include<cstdio>
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     return 0;
+//     int num, time;
+//     double salary;
+//     cin >> num >> time >> salary;
+//     printf("NUMBER = %d\nSALARY = U$ %.2lf", num, salary * time);
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << a + b << endl;
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int a, b;
+//         cin >> a >> b;
+//         cout << a + b << endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int a, b;
+//     while (cin >> a >> b) {
+//         cout << a + b << endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main() {
+//     int a, b;
+//     while (cin >> a >> b, a || b) {
+//         cout << a + b << endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int Plu(int n) {
+//     if (n <= 1) {
+//         return 1;
+//     }
+//     else return Plu(n - 1) * n;
+// }
+// int main() {
+//     int n;
+//     cin >> n;
+//     int x = Plu(n);
+//     cout << x << endl;
+//     return 0;
+// }
