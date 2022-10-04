bf0207d20b0339e1b91b9d96deacb61e788fa834
ColemanTao <109811068+ColemanTao@users.noreply.github.com>
2022-10-04 20:18:15 +0800

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+
+// using namespace std;
+
+// const int N = 1e5;
+// int n;
+// int q[N];
+
+// void quick_sort(int q[], int l, int r) {
+//     if (l >= r) return;
+//     int x = q[l + r >> 1], i = l - 1, j = r + 1;
+//     while (l < r) {
+//         while (q[++i] < x);
+//         while (q[--j] > x);
+//         if (l < r) {
+//             swap(q[i], q[j]);
+//         }
+//         quick_sort(q, l, j);
+//         quick_sort(q, j + 1, r);
+//     }
+// }
+// int main() {
+//     cin >> n;
+//     for (int i = 0 ; i < n; i++) {
+//         cin >> q[i];
+//     }
+//     quick_sort(q, 0, n - 1);
+//     for (int i = 0; i < n; i++) {
+//         cout << q[i] << ' ';
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    return 0;
+}
